955455a481b93241eb5039c8cd201115ac60aeae
k-zhou <kejin.zhou@aalto.fi>
2025-02-06 09:36:32 +0200

    fix 2

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.get("/courses", (c) => c.json(
   {"courses": [ {"id": 1, "name": "Web Software Development" }, {"id": 2, "name": "Device-Agnostic Design" } ] }
 ))
 app.get("/courses/:id", (c) => c.json(
-  {"course":{"id":c.req.param("id"), "name": "Course Name"}}
+  {"course":{"id":Number(c.req.param("id")), "name": "Course Name"}}
 ))
 app.post("/courses", async (c) => {
   const obj = await c.req.json();
@@ -31,7 +31,7 @@ app.get("/courses/:cId/topics/:tId/posts", (c) => c.json(
   {"posts": [ {"id": 1, "title": "Post 1" }, {"id": 2, "title": "Post 2" } ] }
 ))
 app.get("/courses/:cId/topics/:tId/posts/:pId", (c) => c.json(
-  {"post": {"id": c.req.param("pId"), "title": "Post Title" }, "answers": [ { "id": 1, "content": "Answer 1" }, {"id": 2, "content": "Answer 2" } ] }
+  {"post": {"id": Number(c.req.param("pId")), "title": "Post Title" }, "answers": [ { "id": 1, "content": "Answer 1" }, {"id": 2, "content": "Answer 2" } ] }
 ))
 
 export default app;
