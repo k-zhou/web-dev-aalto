f0cd6ca4e237cba90583c370f11a049ee37a48d1
Kejin Zhou <kejin.zhou@aalto.fi>
2025-02-18 12:03:33 +0200

    finishes exercise fetch API - Pokemon

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script>
   import Spells from "$lib/components/Spells.svelte";
+  import PokemonCards from "$lib/components/PokemonCards.svelte";
 </script>
 
+<br />
 <h1>Spells</h1>
 <Spells />
+<br />
+<PokemonCards />
