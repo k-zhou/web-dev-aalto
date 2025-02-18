7e2e803b741d47ca324f126673e6f636b8e78bda
Kejin Zhou <kejin.zhou@aalto.fi>
2025-02-18 11:50:17 +0200

    fixes previous commit on missing files

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,9 +1,6 @@
 <script>
-  import Questions from "$lib/components/Questions.svelte";
   import Spells from "$lib/components/Spells.svelte";
 </script>
 
-<Questions />
-<br />
 <h1>Spells</h1>
 <Spells />
