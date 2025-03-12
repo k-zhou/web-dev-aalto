77b6f0cf5c08aba92f80796c5b7cf872c9b67266
Kejin Zhou <kejin.zhou@aalto.fi>
2025-03-12 11:59:28 +0200

    cleans up questionState

--- client/src/lib/states/questionState.svelte.js
+++ client/src/lib/states/questionState.svelte.js
@@ -6,17 +6,14 @@ let initialQuestions = [];
 // if (browser && localStorage.hasOwnProperty(QUESTIONS_KEY)) {
 //     initialQuestions = JSON.parse(localStorage.getItem(QUESTIONS_KEY));
 // }
-// const res = questionApi.fetch()
-
 let questionState = $state(initialQuestions);
 
-const saveQuestions = () => {
-  localStorage.setItem(QUESTIONS_KEY, JSON.stringify(questionState));
-};
+// const saveQuestions = () => {
+//   localStorage.setItem(QUESTIONS_KEY, JSON.stringify(questionState));
+// };
 
 const fetchQuestions = async () => {
   const data = await questionApi.getQuestions();
-    // console.log(data); // debug
     if (data) {
       questionState = data;
     }
@@ -29,35 +26,25 @@ const useQuestionState = () => {
     },
     fetch: async () => {
       await fetchQuestions();
-      // saveQuestions();
     },
     add: async (q) => {
       const data = await questionApi.postQuestion(q);
-      // console.log(data); // debug
       if (data) {
         await fetchQuestions();
       }
-      // saveQuestions();
     },
     upvote: async (id) => {
       const q = questionState.find((q) => q.id === id);
       const data = await questionApi.upvoteQuestion(q.id);
       if (data) {
-        // questionState.forEach((q) => q.id === data.id ? q = data : {} ); // doesn't change the state apparantly
-        // const i = questionState.findIndex((q) => q.id === data.id);
-        // questionState[i] = data;
         await fetchQuestions();
       }
-      // saveQuestions();
     },
     remove: async (id) => {
       const data = await questionApi.deleteQuestion(id);
-      // console.log("remove, data:", data);
       if (data) {
-        // questionState = questionState.filter((q) => q.id !== data.id);
         await fetchQuestions();
       }
-      // saveQuestions();
     },
   };
 };
