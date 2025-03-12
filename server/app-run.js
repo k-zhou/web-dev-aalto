9fe6f7d40d835ef5e574d4d1c94b1fc285e50adb
Kejin Zhou <kejin.zhou@aalto.fi>
2025-03-12 11:48:48 +0200

    changes the server to use certs

--- server/app-run.js
+++ server/app-run.js
@@ -1,3 +1,8 @@
 import app from "./app.js";
 
-Deno.serve(app.fetch);
+Deno.serve(
+{
+    cert: Deno.readTextFileSync("./cert/server-cert.pem"),
+    key:  Deno.readTextFileSync("./cert/server-key.pem"),
+}, 
+app.fetch);
