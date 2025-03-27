6bfc5fd60f60cf1b11b189b2fe926e6f5646d0ea
Kejin Zhou <kejin.zhou@aalto.fi>
2025-03-27 12:59:14 +0200

    ex. sets up server to use university temporary database

--- server/app.js
+++ server/app.js
@@ -4,79 +4,47 @@ import { logger } from "@hono/hono/logger";
 import postgres from "postgres";
 
 const app = new Hono();
-const sql = postgres();
 
-let questionsList = [];
-// test examples
-// const ex1 = { "id": 1, "title": "Question 1 title", "text": "Question 1 text", "upvotes": 0 };
-// const ex2 = { "id": 2, "title": "Question 2 title", "text": "Question 2 text", "upvotes": 2 };
-// questionsList.push(ex1);
-// questionsList.push(ex2);
+const BANNED_WORDS = [
+  "delete", "update", "insert", "drop", "alter", "create",
+  "truncate", "replace", "merge", "grant", "revoke",
+  "transaction", "commit", "rollback", "savepoint", "lock",
+  "execute", "call", "do", "set", "comment"
+];
+const query = async (query) => {
+  // check that the query does not do data manipulation
+  for (const word of BANNED_WORDS) {
+    if (query.toLowerCase().includes(word)) {
+      throw new Error(`You cannot ${word} data`);
+    }
+  }
 
-app.use("/*", cors());
-app.use("/*", logger());
+  const sql = postgres({
+    max: 2,
+    max_lifetime: 10,
+  });
+  return await sql.unsafe(query);
+};
 
-app.get("/courses", (c) => {
-  return c.json({"courses": [ {"id": 1, "name": "Web Software Development" }, {"id": 2, "name": "Device-Agnostic Design" } ] });
+app.get("/*", (c) => {
+  return c.html(`
+    <html>
+      <head>
+        <title>Hello, world!</title>
+      </head>
+      <body>
+        <h1>Hello, world!</h1>
+        <p>To use this, make a POST with a JSON document in the request body. The query property of the JSON document will be used to query a database.</p>
+        <p>There are no tables though, so you can only do simple queries like "SELECT 1 + 1".</p>
+      </body>
+    </html>
+    `);
 });
 
-app.get("/courses/:id", (c) => {
-  return c.json({"course":{"id":Number(c.req.param("id")), "name": "Course Name"}});
+app.post("/*", async (c) => {
+  const body = await c.req.json();
+  const result = await query(body.query);
+  return c.json({"result": result});
 });
-
-app.post("/courses", async (c) => {
-  const obj = await c.req.json();
-  return c.json({"course":{"id": 3, "name": obj.name}});
-});
-
-app.get("/courses/:id/questions", (c) => {
-  return c.json(questionsList);
-});
-
-// Receives a new question, adds it to the list, and responds with the new entry
-app.post("/courses/:id/questions", async (c) => {
-  const received = await c.req.json();
-  // NOTICE this could currently lead to repeated id:s
-  let newQuestion = {
-    "id": questionsList.length + 1, 
-    "title": received.title,
-    "text": received.text,
-    "upvotes": 0
-  };
-  questionsList.push(newQuestion);
-  return c.json(newQuestion);
-});
-
-// Receives an upvote to a question, records the upvote and responds with the updated question
-app.post("/courses/:id/questions/:qId/upvote", (c) => {
-  const i = questionsList.findIndex((q) => q.id === Number(c.req.param("qId")));
-  questionsList[i]["upvotes"]++;
-  return c.json(questionsList[i]);
-});
-
-// NOTICE this could currently lead to repeated id:s
-// Removes the question pointed to by qID
-app.delete("/courses/:id/questions/:qId", (c) => {
-  const removed = questionsList.find((q) => q.id === Number(c.req.param("qId")));
-  questionsList = questionsList.filter((q) => q.id !== Number(c.req.param("qId")));
-  return c.json(removed);
-});
-
-// This section is for an exercise round
-import * as feedbackRepository from "./feedbackRepository.js";
-
-app.get("/feedbacks/:grading", async (c) => {
-  return c.json({ "count": await feedbackRepository.get(c.req.param("grading")) });
-});
-
-app.post("/feedbacks/:grading", async (c) => {
-  return c.json({ "count": await feedbackRepository.update(c.req.param("grading")) });
-});
-
-
-// Template
-// app.get("", (c) => {
-
-// });
 
 export default app;
