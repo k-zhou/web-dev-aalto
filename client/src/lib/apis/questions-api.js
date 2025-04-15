b6d532674838cfe2ab9a3c384cba000592f356ca
Kejin Zhou <kejin.zhou@aalto.fi>
2025-04-15 11:55:25 +0300

    refactors questions-api

--- client/src/lib/apis/questions-api.js
+++ client/src/lib/apis/questions-api.js
@@ -1,29 +1,5 @@
 import { PUBLIC_API_URL } from "$env/static/public";
-
-// with timeout and error, returns a {data, error}
-const fetchWithErrorHandling = async (url, options) => {
-    const timeoutMs = 5000;
-    try {
-        const response = await fetch(
-            url,
-            {
-                signal: AbortSignal.timeout(timeoutMs),
-                ...options,
-            },
-        );
-        if (response.ok) {
-            const data = await response.json();
-            return {data: data, error: null};
-        }
-        else
-            return {data: null, error: response.statusText};
-    } 
-    catch (error) {
-        return {data: null, error: error};
-    }
-    
-    
-};
+import { fetchWithErrorHandling } from "$lib/apis/fetchWithErrorHandling";
 
 const getQuestions = async (id) => {
     const response = await fetchWithErrorHandling(
