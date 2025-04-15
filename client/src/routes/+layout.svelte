18cc68acc109c4f7a0badbc84930ef637a608c57
Kejin Zhou <kejin.zhou@aalto.fi>
2025-04-15 13:09:21 +0300

    adds layout file

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import "../app.css";
+  let { children } = $props();
+  export const prerender = true;
+</script>
+
+<div class="flex flex-col h-full">
+  <header class="flex items-center bg-primary-100 p-4 mb-6">
+    <h1 class="flex text-xl text-primary-900">Courses and Questions</h1>
+    <nav>
+      <ul class="flex space-x-4 ml-4">
+        <li><a class="anchor" href="/">Home</a></li>
+        <li><a class="anchor" href="/courses">Courses</a></li>
+        <!-- <li><a class="anchor" href="/contact">Contact</a></li> -->
+      </ul>
+    </nav>
+  </header>
+  
+  <main class="container mx-auto max-w-2xl grow">
+    {@render children()}
+  </main>
+  
+  <footer class="bg-primary-100 p-4 border-t-2 border-gray-300">
+    <p class="text-center text-primary-900">Aalto Web Software Development 2025 Spring</p>
+  </footer>  
+</div>
