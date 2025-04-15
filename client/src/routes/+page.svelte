6ba9fb4d6abc9c93d526c61f871aba34280304ac
Kejin Zhou <kejin.zhou@aalto.fi>
2025-04-15 13:06:36 +0300

    changes homepage

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<script>
-  import Questions from "$lib/components/Questions.svelte";
-</script>
-
-<Questions />
+<!-- Homepage -->
+<span class="flex flex-col space-y-2 bg-gray-700 rounded-[0.5vw] p-2">
+  <h1 class="h1 text-center">Welcome!</h1>
+  <a href="/courses" class="btn preset-filled-primary-500">Courses</a>
+</span>
