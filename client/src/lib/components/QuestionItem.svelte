affea08c14e10344e8f24e885942fb059fdbc354
Kejin Zhou <kejin.zhou@aalto.fi>
2025-04-15 17:02:45 +0300

    adds styling to QuestionItem

--- client/src/lib/components/QuestionItem.svelte
+++ client/src/lib/components/QuestionItem.svelte
@@ -7,10 +7,10 @@
   <span class="flex space-x-2 p2">
     <h3 class="h3 grow">{question.title}</h3>
     <p>Upvotes: {question.upvotes}</p>
-    <button onclick={() => questionState.upvote(question.id)}>Upvote</button>
+    <button onclick={() => questionState.upvote(question.id)} class="btn preset-outlined-primary-500">Upvote</button>
     <br />
-    <button onclick={() => questionState.remove(question.id)}>Delete</button>
+    <button onclick={() => questionState.remove(question.id)} class="btn preset-outlined-error-500">Delete</button>
   </span>
-  <p class="w-full">{question.text}</p>
+  <p class="w-full preset-filled-surface-500 rounded-[0.5vw] p-2">{question.text}</p>
   <hr>
 </span>
