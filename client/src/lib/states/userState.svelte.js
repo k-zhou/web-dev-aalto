9ef026358c5886aa8e8256ac9690ca20b1bbf5a6
Kejin Zhou <kejin.zhou@aalto.fi>
2025-05-22 13:57:27 +0300

    adds userState.svelte.js

---
+++ client/src/lib/states/userState.svelte.js
@@ -0,0 +1,15 @@
+
+let user = $state({});
+
+const useUserState = () => {
+  return {
+    get user() {
+      return user;
+    },
+    set user(u) {
+      user = u;
+    },
+  };
+};
+
+export { useUserState };
